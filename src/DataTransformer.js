4ae8a2eb8135a4ef26aa540da8030218c0ddc0eb
Rafa Mateo <rafael.mateo@atrapalo.com>
2018-01-05 12:57:48 +0100

    eslint

--- src/DataTransformer.js
+++ src/DataTransformer.js
@@ -1,88 +1,89 @@
 const TRANSPORT_TYPE = 'TRANSPORT';
 
 class DataTransformer {
-  transform(data) {
-    let flightResults = ((combinations) => {
-      return combinations.reduce((prev, combination) => {
-        const identity = combination.identity;
-        const type = combination.type === TRANSPORT_TYPE ? 'transports' : 'packages';
-        prev[identity] = {
-          type: combination.type
-        };
-
-        if (type === 'packages') {
-          combination[type].forEach((_package) => {
-            let transports = _package.transports;
-            Object.keys(transports).forEach((key) => {
-              transports[key].forEach((transport) => {
-                prev[identity][transport.id] = {
-                  provider: transport.provider,
-                  id: transport.id,
-                  package_identity: _package.identity,
-                  type: combination.type,
-                  plating_carrier: transport.plating_carrier,
-                  price_lines: transformPriceLines(_package.price_lines)
+    transform(data) {
+        let flightResults = ((combinations) => {
+            return combinations.reduce((prev, combination) => {
+                const identity = combination.identity;
+                const type = combination.type === TRANSPORT_TYPE ? 'transports' : 'packages';
+                prev[identity] = {
+                    type: combination.type
                 };
-              });
-            });
-          });
 
-          return prev;
-        }
+                if (type === 'packages') {
+                    combination[type].forEach((_package) => {
+                        let transports = _package.transports;
+                        Object.keys(transports).forEach((key) => {
+                            transports[key].forEach((transport) => {
+                                prev[identity][transport.id] = {
+                                    provider: transport.provider,
+                                    id: transport.id,
+                                    package_identity: _package.identity,
+                                    type: combination.type,
+                                    plating_carrier: transport.plating_carrier,
+                                    price_lines: transformPriceLines(_package.price_lines)
+                                };
+                            });
+                        });
+                    });
 
-        Object.keys(combination[type]).forEach((key) => {
-          combination[type][key].forEach((transport) => {
-            prev[identity][transport.id] = {
-              provider: transport.provider,
-              id: transport.id,
-              type: combination.type,
-              plating_carrier: transport.plating_carrier,
-              price_lines: transformPriceLines(transport.price_lines)
-            };
-          });
-        });
+                    return prev;
+                }
 
-        return prev;
-      }, {});
-    })(data.data.combinations);
+                Object.keys(combination[type]).forEach((key) => {
+                    combination[type][key].forEach((transport) => {
+                        prev[identity][transport.id] = {
+                            provider: transport.provider,
+                            id: transport.id,
+                            type: combination.type,
+                            plating_carrier: transport.plating_carrier,
+                            price_lines: transformPriceLines(transport.price_lines)
+                        };
+                    });
+                });
 
-    return {...data, flightResults};
-  }
+                return prev;
+            }, {});
+        })(data.data.combinations);
+
+        return {...data, flightResults};
+    }
 }
 
 function reduceToUniquePriceLines(priceLines) {
-  let uniqueLines = [];
-  let tempLines = {};
+    let uniqueLines = [];
+    let tempLines = {};
 
-  priceLines.forEach((priceLine) => {
-    let key = `${priceLine.price.amount}${priceLine.price.currency}${priceLine.type}${priceLine.payment_method ? priceLine.payment_method : ''}`;
+    priceLines.forEach((priceLine) => {
+        let key = `${priceLine.price.amount}${priceLine.price.currency}${priceLine.type}${priceLine.payment_method ? priceLine.payment_method : ''}`;
 
-    if (tempLines[key]) {
-      tempLines[key] = {...priceLine, quantity: tempLines[key].quantity += priceLine.quantity};
-    } else {
-      tempLines[key] = {...priceLine};
-    }
-  });
+        if (tempLines[key]) {
+            tempLines[key] = {...priceLine, quantity: tempLines[key].quantity += priceLine.quantity};
+        } else {
+            tempLines[key] = {...priceLine};
+        }
+    });
 
-  Object.keys(tempLines).forEach((key) => {
-    uniqueLines.push(tempLines[key]);
-  });
+    Object.keys(tempLines).forEach((key) => {
+        uniqueLines.push(tempLines[key]);
+    });
 
-  return uniqueLines;
+    return uniqueLines;
 }
 
 function transformPriceLines(priceLines) {
-  let lines = {};
+    let lines = {};
 
-  reduceToUniquePriceLines(priceLines).forEach((line) => {
-    if (!lines[line.type]) {
-      lines[line.type] = [];
-    }
+    reduceToUniquePriceLines(priceLines).forEach((line) => {
+        if (!lines[line.type]) {
+            lines[line.type] = [];
+        }
 
-    lines[line.type].push(`${line.price.amount}|${line.price.currency}|${line.quantity}|${line.payment_method ? line.payment_method : ''}`);
-  });
+        lines[line.type].push(`${line.price.amount}|${line.price.currency}|${line.quantity}|${line.payment_method ? line.payment_method : ''}`);
+    });
 
-  return lines;
+    return lines;
 }
 
+export default DataTransformer;
 export default DataTransformer;
